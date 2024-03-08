d0ef97cb5f17197d26118c9c213df2ff1bd33452
Connor <hoanganhbr03@gmail.com>
2024-03-08 12:18:54 +0700

    begging

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  <Analytics />
+  <Analytics debug={false}/>
     <App />
   </>,
 )
