560627c3a84fdaab012af514abe2dd43aeb12839
Connor <hoanganhbr03@gmail.com>
2024-03-08 12:12:56 +0700

    please work

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
+  <Analytics />
     <App />
-    <Analytics />
-  </React.StrictMode>,
+  </>,
 )
