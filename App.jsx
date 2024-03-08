03480eca2270f1a270164209243023a3d0f4309e
Connor <hoanganhbr03@gmail.com>
2024-03-08 11:56:56 +0700

    analytics

--- App.jsx
+++ App.jsx
@@ -7,45 +7,7 @@ import Projects from "./src/pages/Projects";
 import ScrolToTop from "./public/ScrolToTop";
 import Contact from "./src/pages/Contact";
 import { useEffect } from "react";
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDx50ENibTdPecO26fY_v49_EmxSPOQBe8",
-
-  authDomain: "portfolio-9e9d2.firebaseapp.com",
-
-  projectId: "portfolio-9e9d2",
-
-  storageBucket: "portfolio-9e9d2.appspot.com",
-
-  messagingSenderId: "704691374197",
-
-  appId: "1:704691374197:web:44b88d5978f164536f903c",
-
-  measurementId: "G-RYVCE05ZDE"
-
-};
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
     useEffect(() => {
@@ -61,6 +23,7 @@ export default function App() {
     return (
         <>
         <Router>
+        <Analytics />
         <Nav />
         <ScrolToTop />
         <Routes>
